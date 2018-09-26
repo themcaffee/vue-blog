ad0ff87e2aea68786a4094dd4e335b413e1fb13e
Mitch McAffee <themcaffee@gmail.com>
2018-09-26 01:03:05 -0600

    Add 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -3,18 +3,19 @@ import Router from 'vue-router'
 
 const Home = () => import('@/pages/Home')
 const ViewSinglePost = () => import('@/pages/ViewSinglePost')
+const Error404 = () => import('@/pages/Error404')
 
 
 Vue.use(Router)
 
 const routes = [
   { path: '/', name: 'root', component: Home },
-  { path: '/post/:postname', name: 'post', component: ViewSinglePost }
+  { path: '/posts/:postname', name: 'post', component: ViewSinglePost },
+  { path: '*', component: Error404 }
 ]
 
 const router = new Router({
   mode: 'history',
-  base: '/control/',
   routes
 })
 
